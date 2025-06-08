db39743845797076734955a4d1f569a7a742432d
Wesley Balbino <wesleybalb@gmail.com>
2025-06-08 18:37:05 -0300

    inutilizando a função que seta um usuário logado para fins de testes

--- src/profile/src/script.js
+++ src/profile/src/script.js
@@ -1,13 +1,13 @@
-const user = JSON.parse(localStorage.getItem("LogedUser"));
-  const defaultUser = {
-            "name": "WesleyBalbino",
-            "realname": "Wesley Pinheiro Balbino",
-            "tipo": "Admin",
-            "curso": "Sistemas de Informação",
-            "email": "202420243@unifoa.edu.br"
-            }
-    if (!user) {
-        localStorage.setItem("LogedUser", JSON.stringify([defaultUser]));
-    } else {
-        console.log("User is already set:", user);
-    }
+// const user = JSON.parse(localStorage.getItem("LogedUser"));
+//   const defaultUser = {
+//             "name": "WesleyBalbino",
+//             "realname": "Wesley Pinheiro Balbino",
+//             "tipo": "Admin",
+//             "curso": "Sistemas de Informação",
+//             "email": "202420243@unifoa.edu.br"
+//             }
+//     if (!user) {
+//         localStorage.setItem("LogedUser", JSON.stringify([defaultUser]));
+//     } else {
+//         console.log("User is already set:", user);
+//     }
