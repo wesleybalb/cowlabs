a90bb5cc7b5d87f67d3a7e65782aedb7bd5205a3
wesleybalb <wesleybalb@gmail.comm>
2026-04-01 19:57:19 -0300

    restauração da página de perfil para o modelo antigo sem react. Falta integrar com as rotas de backend

--- src/profile/script.js
+++ src/profile/script.js
@@ -1,13 +1,47 @@
-// const user = JSON.parse(localStorage.getItem("LogedUser"));
-//   const defaultUser = {
-//             "name": "WesleyBalbino",
-//             "realname": "Wesley Pinheiro Balbino",
-//             "tipo": "Admin",
-//             "curso": "Sistemas de Informação",
-//             "email": "202420243@unifoa.edu.br"
-//             }
-//     if (!user) {
-//         localStorage.setItem("LogedUser", JSON.stringify([defaultUser]));
-//     } else {
-//         console.log("User is already set:", user);
-//     }
+const imageInput = document.getElementById('image-input');
+const profileImage = document.getElementById('profile-image');
+
+imageInput.addEventListener('change', function () {
+  const file = this.files[0];
+  if (file) {
+    const reader = new FileReader();
+    reader.onload = function (e) {
+      profileImage.src = e.target.result;
+    };
+    reader.readAsDataURL(file);
+  }
+});
+
+const stars = document.querySelectorAll('#rating i');
+  const ratingValue = document.getElementById('rating-value');
+  let currentRating = 0;
+
+  function updateStars(rating) {
+    stars.forEach(star => {
+      const value = parseInt(star.getAttribute('data-value'));
+      if (value <= rating) {
+        star.classList.remove('bi-star');
+        star.classList.add('bi-star-fill', 'text-warning');
+      } else {
+        star.classList.add('bi-star');
+        star.classList.remove('bi-star-fill', 'text-warning');
+      }
+    });
+  }
+
+  stars.forEach(star => {
+    star.addEventListener('click', () => {
+      currentRating = parseInt(star.getAttribute('data-value'));
+      ratingValue.textContent = `Nota: ${currentRating}`;
+      updateStars(currentRating);
+    });
+
+    star.addEventListener('mouseover', () => {
+      const hoverRating = parseInt(star.getAttribute('data-value'));
+      updateStars(hoverRating);
+    });
+
+    star.addEventListener('mouseout', () => {
+      updateStars(currentRating);
+    });
+  });
